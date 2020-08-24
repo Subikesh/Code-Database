a396a170a7a709fbeca5d5d3bb4e7315ed244289
Subikesh <subikeshps@gmail.com>
2020-08-24 12:44:57 +0530

    User Registration done

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def homepage(request):
@@ -8,6 +9,28 @@ def homepage(request):
 
 def register(request):
     context = {'register_page': 'active'}
+    if request.method == 'POST':
+        user            = request.POST.get('username')
+        first           = request.POST.get('first_name')
+        last            = request.POST.get('last_name')
+        mail            = request.POST.get('email')
+        password        = request.POST.get('password')
+        if User.objects.filter(email = mail).exists():
+            print("Account exist with this email. Please Login.")
+            return redirect('/login')
+        if User.objects.filter(username = user).exists():
+            print("Username taken")
+            return render(request, "register.html", context)
+        user = User.objects.create_user(
+            username = user,
+            first_name = first,
+            last_name = last, 
+            email = mail,
+            password = password,
+        )
+        user.save()
+        print("User created successfully")
+        return redirect('/')
     return render(request, "register.html", context)
 
 def login(request):
