a639003a8475c7b3fb4645f54edde6029bd7bb14
Subikesh <subikeshps@gmail.com>
2020-08-25 21:48:53 +0530

    Changes in Many-many field

--- main/models.py
+++ main/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+class Tag(models.Model):
+    name            = models.CharField(max_length = 100)
+    description     = models.TextField(null = True, blank = True)
+
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
+    difficulty_choices = [
+        ("Hard", "Hard"),
+        ("Medium", "Medium"),
+        ("Easy", "Easy"),
+    ]
     user            = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="User", on_delete = models.CASCADE)
     title           = models.CharField(max_length = 200)
-    description     = models.TextField(null = True)
-    link            = models.URLField(null = True)
-    difficulty      = models.TextField(max_length = 10)
-    Other           = models.TextField(null = True)
+    description     = models.TextField(null = True, blank = True)
+    link            = models.URLField(null = True, blank = True)
+    difficulty      = models.TextField(max_length = 10, choices = difficulty_choices)
+    tag             = models.ManyToManyField(Tag)
+    Other           = models.TextField(null = True, blank = True)
 
     def __str__(self):
         return self.title
@@ -17,16 +29,10 @@ class Solution(models.Model):
     question        = models.ForeignKey(Question, verbose_name="Question", on_delete = models.CASCADE)
     title           = models.CharField(max_length = 100)
     language        = models.CharField(max_length = 20)
-    program         = models.TextField(null = True)
-    notes           = models.TextField(null = True)
-    link            = models.URLField(null = True)
+    program         = models.TextField(null = True, blank = True)
+    notes           = models.TextField(null = True, blank = True)
+    link            = models.URLField(null = True, blank = True)
 
     def __str__(self):
         return self.title
 
-class Tag(models.Model):
-    question        = models.ManyToManyField(Question)
-    name            = models.CharField(max_length = 50)
-
-    def __str__(self):
-        return self.name
