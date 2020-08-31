074432dafaa2e3586e886ae95e41e4cd96a59c50
Subikesh <subikeshps@gmail.com>
2020-08-31 22:53:37 +0530

    Change syntax highlighting and font-size

--- static/js/ace_config.js
+++ static/js/ace_config.js
@@ -1,6 +1,10 @@
 // Getting the basic editor set
 var editor = ace.edit("editor");
 
+// Elements for changing language and font-size
+var lang = document.getElementById("lang-select");
+var size = document.getElementById("size-select");
+
 let editorLib = {
     init() {
         // Theme
@@ -13,7 +17,22 @@ let editorLib = {
             enableMultiselect: true,
             fontSize: '18px',
         });
+    },
+    changeLang(newLang) {
+        editor.session.setMode("ace/mode/"+newLang);
+    },
+    changeFontSize(newSize) {
+        editor.setFontSize(newSize);
     }
 };
 
 editorLib.init();
+editorLib.changeLang(lang.value)
+
+
+lang.addEventListener("change", function() {
+    editorLib.changeLang(this.value);
+});
+size.addEventListener("change", function() {
+    editorLib.changeFontSize(parseInt(this.value));
+});
