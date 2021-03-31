857226a007db0c9d35a41bc5ea4a106ce8b1ecd8
Subikesh <subikeshps@gmail.com>
2021-03-31 19:33:47 +0530

    Added unique constraints and related_names

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Tag(models.Model):
-    name            = models.CharField(max_length = 100)
+    name            = models.CharField(max_length = 100, unique= True)
     description     = models.TextField(null = True, blank = True)
 
     def __str__(self):
@@ -21,13 +21,13 @@ class Question(models.Model):
         ("Private", "Private"),
         ("Public", "Public")
     ]
-    user            = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="User", on_delete = models.CASCADE)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="User", on_delete = models.CASCADE, related_name='questions')
     title           = models.CharField(max_length = 200)
     access          = models.CharField(max_length = 10, choices = access_choices, default = "Private")
     description     = models.TextField(null = True, blank = True)
     link            = models.URLField(null = True, blank = True)
     difficulty      = models.TextField(max_length = 10, choices = difficulty_choices)
-    tag             = models.ManyToManyField(Tag)
+    tag             = models.ManyToManyField(Tag, related_name='questions')
     examples        = models.TextField(null = True, blank = True)
     date_added      = models.DateTimeField(auto_now=True)
 
@@ -35,7 +35,7 @@ class Question(models.Model):
         return self.title
 
 class Solution(models.Model):
-    question        = models.ForeignKey(Question, verbose_name="Question", on_delete = models.CASCADE)
+    question        = models.ForeignKey(Question, verbose_name="Question", on_delete = models.CASCADE, related_name='solutions')
     title           = models.CharField(max_length = 100)
     language        = models.CharField(max_length = 20)
     program         = models.TextField(null = True, blank = True)
@@ -43,6 +43,8 @@ class Solution(models.Model):
     link            = models.URLField(null = True, blank = True)
     date_added      = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['question', '-date_added']
+
     def __str__(self):
         return self.title
-
