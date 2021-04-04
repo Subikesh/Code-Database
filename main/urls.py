8a47a8275f5d738c95af99d65b7e4404d39b298e
Subikesh <subikeshps@gmail.com>
2021-04-04 21:53:12 +0530

    Transfering api functionality to API app

--- main/urls.py
+++ main/urls.py
@@ -21,12 +21,4 @@ urlpatterns = [
     path('questions/<int:question_id>/', views.view_question, name="view_question"),
     path('questions/<int:question_id>/<int:solution_id>/', views.view_question, name="edit_solution"),
     path('delete_solution/<int:question_id>/<int:solution_id>/', views.delete_solution, name="delete_solution"),
-
-    # API urls
-    path('api/questions/', views.QuestionList.as_view(), name="api_questions"),
-    path('api/questions/<int:pk>/', views.QuestionRetrieve.as_view(), name="api_view_question"),
-    path('api/solutions/<int:question_id>/', views.SolutionList.as_view(), name="api_solution"),
-    path('api/solutions/<int:question_id>/<int:pk>/', views.SolutionRetrieve.as_view(), name="api_solution_update"),
-    path('api/tag/', views.CreateTag.as_view(), name="api_create_tag"),
-    path('api/tag/<int:pk>/', views.DeleteTag.as_view(), name="api_delete_tag"),
 ]
