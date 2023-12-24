5e677cdbd250d327ee26d4bcd3ce0662172cecf5
subikesh <subikeshps@gmail.com>
2023-12-24 20:15:38 +0530

    Hosting changes

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, permissions
 from rest_framework.exceptions import ValidationError
 from .serializers import QuestionSerializer, SolutionSerializer, TagSerializer
 
+
 # Custom permission to check if the question's owner is trying to update the question
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -12,6 +13,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         # If current user is the object user
         return obj.user == request.user
+
 
 # List all the public questions and user-private questions
 class QuestionList(generics.ListCreateAPIView):
@@ -27,21 +29,24 @@ class QuestionList(generics.ListCreateAPIView):
         return query
 
     def perform_create(self, serializer):
-        serializer.save(user = self.request.user) 
+        serializer.save(user=self.request.user)
+
 
 # Retrieve, Update and delete a question from given pk
 class QuestionRetrieve(generics.RetrieveUpdateDestroyAPIView):
     # queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly&IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly & IsOwnerOrReadOnly]
 
     def get_queryset(self):
         # If user views anonymously, public questions are shown
         query = Question.objects.filter(pk=self.kwargs.get('pk'))
-        if query.filter(access="Public").exists() or (self.request.user.is_authenticated and query.filter(user=self.request.user).exists()):
+        if query.filter(access="Public").exists() or (
+                self.request.user.is_authenticated and query.filter(user=self.request.user).exists()):
             return query
         else:
             return None
+
 
 # Create a new tag from API call with ../api/tag/
 class CreateTag(generics.ListCreateAPIView):
@@ -55,11 +60,13 @@ class CreateTag(generics.ListCreateAPIView):
         if serializer.is_valid():
             serializer.save()
 
+
 # Delete a tag from pk as url
 class DeleteTag(generics.RetrieveDestroyAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAdminUser]
+
 
 # List all the solutions for a question and create new solution
 class SolutionList(generics.ListCreateAPIView):
@@ -69,20 +76,21 @@ class SolutionList(generics.ListCreateAPIView):
     def get_queryset(self):
         question = Question.objects.get(pk=self.kwargs.get('question_id'))
         print(question)
-        return Solution.objects.filter(question = question)
+        return Solution.objects.filter(question=question)
 
     def perform_create(self, serializer):
         question = Question.objects.get(pk=self.kwargs.get('question_id'))
         if question.user != self.request.user:
             raise ValidationError("Check the question id in url")
-        serializer.save(question = question)
+        serializer.save(question=question)
+
 
 # Retrieve a specific solution and update or delete it
 class SolutionRetrieve(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SolutionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly&IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly & IsOwnerOrReadOnly]
 
     def get_queryset(self):
         question = Question.objects.get(pk=self.kwargs.get('question_id'))
         print(question)
-        return Solution.objects.filter(question = question)
+        return Solution.objects.filter(question=question)
--- code_database/settings.py
+++ code_database/settings.py
@@ -13,23 +13,24 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import django_heroku
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'code-database.herokuapp.com']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -78,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'code_database.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -86,13 +86,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Coding',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -112,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -126,16 +125,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-django_heroku.settings(locals())
